848fb6f032b03daa79d247a34f8259ad43bbc7ae
lgw2238 <nihao3634@naver.com>
2023-12-20 16:20:58 +0900

    kafka-producer 1 commit

---
+++ src/test/kotlin/org/lgw/kafkaproducer/KafkaProducerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.lgw.kafkaproducer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaProducerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
